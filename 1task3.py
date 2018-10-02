37dae4528040af09c7d5d9792c4dd958bd911268
TawsifAhmad <43748084+TawsifAhmad@users.noreply.github.com>
2018-10-02 17:59:42 -0300

    Add files via upload

---
+++ 1task3.py
@@ -0,0 +1,12 @@
+"""function to create a list of "access-list" for "global_access" and "fw-management_access_in"""
+
+def access_list(file_name):
+	file=open(file_name)
+	list=[]
+	for line in file:
+		line=line.strip()
+		for i in line.split():	#if line contains global-access or fw-management it will add that line to list
+			if i=='global_access' or i=='fw-management_access_in':
+				list.append(line)
+	return list
+print(access_list('running-config.cfg')		
